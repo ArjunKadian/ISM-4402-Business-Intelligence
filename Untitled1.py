501e1a1afecd3272f1a4d18265c9f58ca5f0de14
Arjun Kadian <ArjunKadian@users.noreply.github.com>
2020-08-24 20:25:00 -0400

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+Location = "/Users/arjunkadian/Desktop/datasets/smallgradesh.csv" 
+df = pd.read_csv(Location, header=None)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
